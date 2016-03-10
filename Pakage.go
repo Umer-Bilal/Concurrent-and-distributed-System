f4c2aace99b836aa437d54ddd2ed75576c57b8ac
UMER BILAL <UMER BILAL>
2016-03-10 23:39:54 +0500

    pakage assignmnt commit

---
+++ Pakage.go
@@ -0,0 +1,16 @@
+//Package Startinggo shows the how to start go
+package Startinggo
+
+//GreetingsString is a global variable
+var startinggo String = "My first pakage of concurnet and distributed system"
+var helogo string ="Umer first pakage"
+//Printstartinggo is a global function
+func PrintStartinggo(name string) string {
+	return startinggoString + "-" + name
+}
+
+
+func myname(name string) string {
+
+	return helogostring +"-"+ name
+}
